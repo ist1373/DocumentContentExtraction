bf4ef8ef135f1457666b935dbeecc9756843beee
Iman Saberi <iman@charli.ai>
2024-12-27 02:25:20 -0800

    tesseract has been tested.

--- src/document_content_extraction/document_content_extraction.py
+++ src/document_content_extraction/document_content_extraction.py
@@ -1,4 +1,18 @@
+from PIL import Image
+import pytesseract
 
 class DocumentContentExtraction:
     def __init__(self):
         pass
+
+
+if __name__=="__main__":
+
+
+
+    pytesseract.pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"
+
+    # OCR processing
+    image = Image.open('/Users/imansaberi/Documents/research_projects/content_extraction/VGT/high-res-output/page_0.png')
+    text = pytesseract.image_to_string(image)
+    print(text)
