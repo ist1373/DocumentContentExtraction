6d6118a7723c7512e4a37c8f7806c5baef5c226e
Iman Saberi <iman@charli.ai>
2024-12-27 17:48:39 -0800

    extract contents has been implemented for scene.

--- app.py
+++ app.py
@@ -2,6 +2,45 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from subprocess import run, CalledProcessError
 import os
-from DCE.src.document_content_extraction.document_content_extraction import DocumentContentExtraction
+from src.document_content_extraction.document_content_extraction import DocumentContentExtraction
+from src.document_content_extraction.scene import Scene
 from fastapi.responses import ORJSONResponse
 import logging
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+
+app = FastAPI()
+document_content_extractor = DocumentContentExtraction()
+
+class SceneModel(BaseModel):
+    components: list[list[float]]
+    x1: float
+    y1: float
+    x2: float
+    y2: float
+    zoom_factor:float
+
+class ScenesModel(BaseModel):
+    scenes:list[SceneModel]
+    input_image:str
+
+
+@app.post("/extract-contents/")
+def plan_scences(scene_args: ScenesModel):
+    try:
+        scenes = []
+        for scene in scene_args.scenes:
+            scenes.append(Scene(scene.x1,scene.y1,scene.x2,scene.y2,scene.components,scene.zoom_factor))
+
+        scenes = document_content_extractor.process(scene_args.input_image,scenes)
+
+        return {"status": "success", "output": scenes}
+    except CalledProcessError as e:
+        raise HTTPException(status_code=500, detail=f"Error: {e.stderr}")
+
+if __name__ == "__main__":
+
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8002)
--- src/document_content_extraction/document_content_extraction.py
+++ src/document_content_extraction/document_content_extraction.py
@@ -1,9 +1,22 @@
 from PIL import Image
 import pytesseract
+from src.document_content_extraction.scene import Scene
 
 class DocumentContentExtraction:
     def __init__(self):
         pass
+
+    def process(self,input_image,scenes:list[Scene]):
+        # Crop the image to the bounding box
+        image = Image.open(input_image)
+        for scene in scenes:
+            contents = []
+            for comp in scene.components:
+                cropped_image = image.crop(comp)
+                contents.append(pytesseract.image_to_string(cropped_image))
+            scene.content = contents
+        return scenes
+        
 
 
 if __name__=="__main__":
